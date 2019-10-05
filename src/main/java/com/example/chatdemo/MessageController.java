2f1413f58e2f29eeba1e096b88eadbc81cd25694
Thai Le <lnthai2002@gmail.com>
2019-10-04 22:05:45 -0400

    add username and room when to ws session when JOIN msg received

--- src/main/java/com/example/chatdemo/MessageController.java
+++ src/main/java/com/example/chatdemo/MessageController.java
@@ -1,8 +1,10 @@
 package com.example.chatdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
@@ -15,10 +17,15 @@ public class MessageController {
 
     @MessageMapping("/chat/{room}")
     @SendTo("/broadcast/{room}")
-    public Message broadcast(Message message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        if (message.getTo() == null || message.getTo().isEmpty()) {
-            return message;
+    public Message broadcast(@DestinationVariable String room,  Message message, SimpMessageHeaderAccessor headerAccessor) throws Exception {
+        if (room.equals(message.getRoom())) {
+            if (message.getTo() == null || message.getTo().isEmpty()) {
+                if (message.getType() == Message.Type.JOIN) {
+                    headerAccessor.getSessionAttributes().put("username", message.getFrom());
+                    headerAccessor.getSessionAttributes().put("room", message.getRoom());
+                }
+                return message;
+            }
         }
         //simpMessagingTemplate.convertAndSend("/topic/fleet/" + fleetId, new Simple(fleetId, driverId));
         return null;//TODO: how to not broadcast
