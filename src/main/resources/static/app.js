eb9706b85eb4e7a8735ca2023cfe39117233445c
Thai Le <lnthai2002@gmail.com>
2019-10-04 21:58:31 -0400

    refactor, send JOIN message to selected room when ws connected, display message when user JOIN and LEAVE

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,36 +1,72 @@
 var stompClient = null;
 
+function connect() {
+    var socket = new SockJS('/gs-guide-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, onConnected, onError);
+}
+
+function onConnected() {
+    var room = $('#room').val();
+    var username = $('#name').val();
+
+    setConnected(true);
+
+    stompClient.subscribe(
+        '/broadcast/' + room,
+        onMessageBroadcasted
+    );
+
+    stompClient.subscribe(
+        '/pm/' + room + '/' + username ,
+        function (msg) {
+            var msgObj =JSON.parse(msg.body);
+            showPmMsg(msgObj.from + ' says: ' + msgObj.body);
+        }
+    );
+
+    // announce your presence
+    stompClient.send(
+        "/app/chat/" + room,
+        {},
+        JSON.stringify({from: username, room: room, type: 'JOIN'})
+    )
+}
+
 function setConnected(connected) {
     $("#connect").prop("disabled", connected);
     $("#disconnect").prop("disabled", !connected);
+
+    //lock room and username
     $("#room").prop("disabled", connected);
     $("#name").prop("disabled", connected);
+
     if (connected) {
         $("#conversation").show();
+        $("#pmconversation").show();
     }
     else {
         $("#conversation").hide();
+        $("#pmconversation").hide();
     }
-    $("#greetings").html("");
+
+    //$("#conversation").html("");
 }
 
-function connect() {
-    var room = $('#room').val();
-    var username = $('#name').val();
-    var socket = new SockJS('/gs-guide-websocket');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/broadcast/' + room , function (msg) {
-            var msgObj =JSON.parse(msg.body);
-            showMsg(msgObj.from + ' says: ' + msgObj.body);
-        });
-        stompClient.subscribe('/pm/' + room + '/' + username , function (msg) {
-            var msgObj =JSON.parse(msg.body);
-            showPmMsg(msgObj.from + ' says: ' + msgObj.body);
-        });
-    });
+function onError(error) {
+}
+
+function onMessageBroadcasted(msg) {
+    var msgObj =JSON.parse(msg.body);
+    if (msgObj.type == 'JOIN') {
+        showMsg(msgObj.from + ' joined');
+    } else if (msgObj.type == 'LEAVE') {
+        showMsg(msgObj.from + ' left');
+    } else if (msgObj.type == 'CHAT') {
+        showMsg(msgObj.from + ' says: ' + msgObj.body);
+    } else {
+        //ignore unknown message type
+    }
 }
 
 function disconnect() {
@@ -48,13 +84,14 @@ function sendName() {
         'room': room,
         'from': $("#name").val(),
         'to': to,
-        'body': $("#msg").val()
+        'body': $("#msg").val(),
+        'type': 'CHAT'
     };
     if (to == null || to == '') {
-        stompClient.send("/app/chat/"+room, {}, JSON.stringify(msg));
+        stompClient.send("/app/chat/" + room, {}, JSON.stringify(msg));
     }
     else {
-        stompClient.send("/pm/" + to, {}, JSON.stringify(msg));
+        stompClient.send("/app/chat/" + room + '/' + to, {}, JSON.stringify(msg));
     }
 }
 
