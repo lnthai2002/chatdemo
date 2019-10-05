38842420c5a20d84075244b42d640aba4e6f2b87
Thai Le <lnthai2002@gmail.com>
2019-10-05 00:05:51 -0400

    ws listener to send LEAVE message to room when user is disconnected

---
+++ src/main/java/com/example/chatdemo/WebSocketEventListener.java
@@ -0,0 +1,37 @@
+package com.example.chatdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Optional;
+
+@Component
+public class WebSocketEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        //nothing
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String room = (String) headerAccessor.getSessionAttributes().get("room");
+        Message msg = new Message(username, room, Message.Type.LEAVE);
+        String roomDestination = "/broadcast/" + room;
+        messagingTemplate.convertAndSend(roomDestination, msg);
+    }
+}
